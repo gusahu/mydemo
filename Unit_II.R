77401326b9cde764618bc88813a0021cfa38f074
gusahu <gusahu18@gmail.com>
2019-09-13 10:03:31 -0300

    Plot of weather stations

--- Unit_II.R
+++ Unit_II.R
@@ -8,3 +8,7 @@ longitud <- c(-116.7, -120.4, -116.7, -113.5, -115.5,
 latitud <- c(45.3, 42.6, 38.9, 42.1, 35.7, 38.9,
              36.2, 39, 41.6, 36.9)
 est_climatic<- cbind(longitud, latitud)
+
+# simular datos de lluvias
+set.seed(0)
+precip <-round((runif(length(latitud))*10)^3)
