6df162b90338047dc793e311626fc298751f17d0
gusahu <gusahu18@gmail.com>
2019-09-13 09:59:56 -0300

    Data simulation

--- Unit_II.R
+++ Unit_II.R
@@ -2,3 +2,9 @@
 library(pacman)
 pacman::p_load(raster, sf, maptools, rgdal, ggplot2, tidyverse, broom)
 
+nombre <- LETTERS[1:10]
+longitud <- c(-116.7, -120.4, -116.7, -113.5, -115.5,
+              -120.8, -119.5, -113.7, -113.7, -110.7)
+latitud <- c(45.3, 42.6, 38.9, 42.1, 35.7, 38.9,
+             36.2, 39, 41.6, 36.9)
+est_climatic<- cbind(longitud, latitud)
